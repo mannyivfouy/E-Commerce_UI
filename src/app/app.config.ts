8cafc7ce03d6bb7386dad0b44d32329a5b59eda1
Mann Yivfouy <mannyivfouy@gmail.com>
2026-03-20 16:34:20 +0700

    add interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,14 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { withInterceptors } from '@angular/common/http';
+import { authInterceptor } from './core/interceptor/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideHttpClient(),
-    provideRouter(routes),    
+    provideHttpClient(withInterceptors([authInterceptor])),
+    provideRouter(routes),
   ],
 };
