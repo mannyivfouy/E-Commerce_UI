b31985d700569536e6d7c51aedb55fbab793b240
Mann Yivfouy <mannyivfouy@gmail.com>
2026-03-23 00:14:10 +0700

    User form edit_create

--- src/app/features/admin/user/user-list/user-list.ts
+++ src/app/features/admin/user/user-list/user-list.ts
@@ -1,10 +1,9 @@
-import { ChangeDetectorRef, Component, OnInit} from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { User } from '../../../../models/user.model';
 import { UserService } from '../../../../core/services/user-service';
 import { CommonModule } from '@angular/common';
 import { Avatar } from '../../../../shared/components/avatar/avatar';
-import { Router } from '@angular/router';
-import { UserForm } from "../user-form/user-form";
+import { UserForm } from '../user-form/user-form';
 
 @Component({
   selector: 'app-user-list',
@@ -44,20 +43,64 @@ export class UserList implements OnInit {
     });
   }
 
-  deleteUser() {}
-
   openAddForm() {
     this.selectedUser = null;
     this.showModal = true;
   }
 
-  openEditForm(user: any) {
-    // this.router.navigate(['/admin/users/edit', user._id]);
+  openEditForm(user: User) {
+    this.selectedUser = user;
+    this.showModal = true;
   }
 
-  closeModal(refresh: boolean = false) {
+  closeModal(result: User | null) {
     this.showModal = false;
-    this.selectedUser = null;
-    if (refresh) this.getUsers(); // refresh list after save
+
+    if (result) {
+      if (this.selectedUser) {
+        // Update existing user
+        this.updateUser(result);
+      } else {
+        // Add new user
+        this.addUser(result);
+      }
+    }
+  }
+
+  addUser(user: User) {
+    this.userService.createUser(user).subscribe({
+      next: (res) => {
+        this.users.push(res);
+        this.cdr.detectChanges();
+      },
+      error: (err) => console.error(err),
+    });
+  }
+  
+  updateUser(user: User) {
+    if (!this.selectedUser?._id) return;
+
+    this.userService.updateUser(this.selectedUser._id, user).subscribe({
+      next: (res) => {
+        const index = this.users.findIndex((u) => u._id === res._id);
+        if (index !== -1) this.users[index] = res;
+        this.cdr.detectChanges();
+      },
+      error: (err) => console.error(err),
+    });
+  }
+
+  deleteUser(user: User) {
+    if (!user._id) return;
+
+    if (!confirm(`Are you sure you want to delete ${user.fullname}?`)) return;
+
+    this.userService.deleteUser(user._id).subscribe({
+      next: () => {
+        this.users = this.users.filter((u) => u._id !== user._id);
+        this.cdr.detectChanges();
+      },
+      error: (err) => console.error(err),
+    });
   }
 }
