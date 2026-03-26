0a3959a821a6329178b67d3f9116b071a5826ea5
Mann Yivfouy <mannyivfouy@gmail.com>
2026-03-26 17:20:41 +0700

    add feature pagination and search in product list

--- src/app/features/admin/product/product-list/product-list.ts
+++ src/app/features/admin/product/product-list/product-list.ts
@@ -2,16 +2,25 @@ import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { Product } from '../../../../models/product.model';
 import { ProductService } from '../../../../core/services/product-service';
 import { CommonModule } from '@angular/common';
+import { Pagination } from '../../../../shared/components/pagination/pagination';
+import { Search } from "../../../../shared/components/search/search";
 
 @Component({
   selector: 'app-product-list',
-  imports: [CommonModule],
+  imports: [CommonModule, Pagination, Search],
   templateUrl: './product-list.html',
   styleUrl: './product-list.css',
 })
 export class ProductList implements OnInit {
   products: Product[] = [];
   loading: boolean = false;
+
+  searchTerm: string = '';
+  filteredProduct: Product[] = [];
+
+  currentPage: number = 1;
+  pageSize: number = 10;
+  pagedProducts: Product[] = [];
 
   constructor(
     private productService: ProductService,
@@ -28,6 +37,9 @@ export class ProductList implements OnInit {
     this.productService.getAllProducts().subscribe({
       next: (res) => {
         this.products = res.products;
+        this.filteredProduct = res.products;
+        this.currentPage = 1;
+        this.updatePagedProducts();
         this.loading = false;
         this.crd.detectChanges();
       },
@@ -36,5 +48,31 @@ export class ProductList implements OnInit {
         this.loading = false;
       },
     });
+  }
+
+  onSearch(value: string) {
+    this.searchTerm = value;
+
+    this.filteredProduct = this.products.filter((product) =>
+      product.productName.toLocaleLowerCase().includes(value.toLowerCase()),
+    );
+
+    this.currentPage = 1;
+    this.updatePagedProducts();
+  }
+
+  updatePagedProducts() {
+    const start = (this.currentPage - 1) * this.pageSize;
+    const end = start + this.pageSize;
+    this.pagedProducts = [...this.filteredProduct.slice(start, end)];
+  }
+
+  onPageChange(page: number) {
+    this.currentPage = page;
+    this.updatePagedProducts();
+  }
+
+  get totalPages(): number {
+    return Math.ceil(this.filteredProduct.length / this.pageSize);
   }
 }
