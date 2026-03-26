19ee86cdbb04ae577bc517ca9e5519c50d001330
Mann Yivfouy <mannyivfouy@gmail.com>
2026-03-26 17:02:14 +0700

    fetch data form API

--- src/app/features/admin/product/product-list/product-list.ts
+++ src/app/features/admin/product/product-list/product-list.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { Product } from '../../../../models/product.model';
+import { ProductService } from '../../../../core/services/product-service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-product-list',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './product-list.html',
   styleUrl: './product-list.css',
 })
-export class ProductList {
+export class ProductList implements OnInit {
+  products: Product[] = [];
+  loading: boolean = false;
 
+  constructor(
+    private productService: ProductService,
+    private crd: ChangeDetectorRef,
+  ) {}
+
+  ngOnInit(): void {
+    this.getProducts();
+  }
+
+  getProducts() {
+    this.loading = true;
+
+    this.productService.getAllProducts().subscribe({
+      next: (res) => {
+        this.products = res.products;
+        this.loading = false;
+        this.crd.detectChanges();
+      },
+      error: (err) => {
+        console.error(err);
+        this.loading = false;
+      },
+    });
+  }
 }
