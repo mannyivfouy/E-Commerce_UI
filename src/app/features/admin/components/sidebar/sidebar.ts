e3d2df556336740d79869db7548696d1c04f4dd9
Mann Yivfouy <mannyivfouy@gmail.com>
2026-04-11 09:45:17 +0700

    fix design sidebar

--- src/app/features/admin/components/sidebar/sidebar.ts
+++ src/app/features/admin/components/sidebar/sidebar.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
-import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { Router, RouterLink, RouterLinkActive, NavigationEnd } from '@angular/router';
 import { AuthService } from '../../../../core/services/auth-service';
+import { AfterViewInit } from '@angular/core';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-sidebar',
@@ -8,14 +10,40 @@ import { AuthService } from '../../../../core/services/auth-service';
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.css',
 })
-export class Sidebar {
+export class Sidebar implements OnInit {
   constructor(
     private router: Router,
     private authService: AuthService,
+    private cdr: ChangeDetectorRef,
   ) {}
+
+  ngOnInit(): void {
+    this.cdr.detectChanges();
+  }
 
   logout() {
     this.authService.logout();
-    this.router.navigate(['/auth'])
+    this.router.navigate(['/auth']);
+  }
+
+  ngAfterViewInit() {
+    this.moveActiveBg();
+
+    this.router.events.pipe(filter((event) => event instanceof NavigationEnd)).subscribe(() => {
+      setTimeout(() => this.moveActiveBg(), 50);
+    });
+  }
+
+  moveActiveBg() {
+    const active = document.querySelector('.nav-link.active') as HTMLElement;
+    const bg = document.querySelector('.active-bg') as HTMLElement;
+    const container = document.querySelector('.sidebar-menu') as HTMLElement;
+
+    if (active && bg && container) {
+      const offset = active.offsetTop - container.offsetTop;
+
+      bg.style.top = offset + 'px';
+      bg.style.height = active.offsetHeight + 'px';
+    }
   }
 }
